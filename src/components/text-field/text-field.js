3cfe72f9c43683236fea0423f9c79d62810a231c
Denis Beschastnykh <staya.bomjei@gmail.com>
2021-10-23 09:59:16 +0500

    text-field component refactoring

--- src/components/text-field/text-field.js
+++ src/components/text-field/text-field.js
@@ -5,6 +5,6 @@ import $ from 'jquery';
 $(
   () => {
     const im = new Inputmask({ alias: 'datetime', inputFormat: 'dd.mm.yyyy', placeholder: '__.__.____' });
-    im.mask($('.text-field_masked'));
+    im.mask($('.js-text-field_masked'));
   },
 );
