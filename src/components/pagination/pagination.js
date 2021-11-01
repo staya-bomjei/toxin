0b10b9a4c8a2c6cf0b545d2bd2504910275d9984
Denis Beschastnykh <staya.bomjei@gmail.com>
2021-11-02 00:13:26 +0500

    Refactoring a pagination components

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -4,24 +4,24 @@ import $ from 'jquery';
 class Pagination {
   constructor($component) {
     this.$component = $component;
-    this.$toLeftButton = $($component).find('.pagination__to-left-button');
-    this.$toRightButton = $($component).find('.pagination__to-right-button');
-    this.$text = $($component).find('.pagination__text');
-    this.numberButtons = Array.from($($component).find('.pagination__number-button'));
-    this.itemsCount = Number($($component).attr('data-items-count'));
-    this.itemsPerPage = Number($($component).attr('data-items-per-page'));
+    this.itemsCount = Number($component.attr('data-items-count'));
+    this.itemsPerPage = Number($component.attr('data-items-per-page'));
     this.pagesCount = Math.ceil(this.itemsCount / this.itemsPerPage);
-    this.postfix = $($component).attr('data-postfix');
+    this.postfix = $component.attr('data-postfix');
+    this.$toLeft = $('.js-pagination__to-left', $component);
+    this.numberButtons = Array.from($('.js-pagination__number', $component));
+    this.$toRight = $('.js-pagination__to-right', $component);
+    this.$text = $('.js-pagination__text', $component);
     this.current = 0;
     this.updateComponent(1);
     this.attachEventHandlers();
   }
 
   attachEventHandlers() {
-    $(this.$toLeftButton).on('click', () => this.onToLeftButtonClick());
-    $(this.$toRightButton).on('click', () => this.onToRightButtonClick());
-    this.numberButtons.forEach(($button) => {
-      $($button).on('click', (event) => this.onNumberButtonClick(event));
+    this.$toLeft.on('click', () => this.onToLeftButtonClick());
+    this.$toRight.on('click', () => this.onToRightButtonClick());
+    this.numberButtons.forEach((button) => {
+      $(button).on('click', (event) => this.onNumberButtonClick(event));
     });
   }
 
@@ -44,11 +44,11 @@ class Pagination {
   }
 
   setNumberButtonCurrent(position) {
-    $(this.numberButtons[position]).addClass('pagination__number-button_current');
+    $(this.numberButtons[position]).addClass('pagination__number_current');
   }
 
   unsetNumberButtonCurrent(position) {
-    $(this.numberButtons[position]).removeClass('pagination__number-button_current');
+    $(this.numberButtons[position]).removeClass('pagination__number_current');
   }
 
   updateButtons(position) {
@@ -57,25 +57,27 @@ class Pagination {
         this.unsetNumberButtonCurrent(i);
       }
     }
+
     this.current = position;
     switch (position) {
       case 1:
-        this.$toLeftButton.hide();
-        this.$toRightButton.show();
+        this.$toLeft.hide();
+        this.$toRight.show();
         break;
       case this.pagesCount:
-        this.$toRightButton.hide();
-        this.$toLeftButton.show();
+        this.$toRight.hide();
+        this.$toLeft.show();
         break;
       default:
-        this.$toRightButton.show();
-        this.$toLeftButton.show();
+        this.$toRight.show();
+        this.$toLeft.show();
     }
 
     const pagination = Pagination.calcPagination(this.pagesCount, position);
     for (let i = 0; i < pagination.length; i += 1) {
       this.setNumberButtonText(i, pagination[i]);
       $(this.numberButtons[i]).show();
+
       if (pagination[i] === position) {
         this.setNumberButtonCurrent(i);
       }
@@ -88,15 +90,18 @@ class Pagination {
   updateText(position) {
     const from = (position - 1) * this.itemsPerPage + 1;
     let to = from + this.itemsPerPage;
+
     if (to > this.itemsCount) {
       to = this.itemsCount;
     }
+
     let over = 1;
     let temp = this.itemsCount;
     while (temp > 10) {
       over += 1;
       temp = Math.trunc(temp / 10);
     }
+
     this.$text.html(`${from} – ${to} из ${temp * 10 ** (over - 1)}+ ${this.postfix}`);
   }
 
