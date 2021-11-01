474b8cc6430f2b4ca4701842ea337708d7f2fd64
Denis Beschastnykh <staya.bomjei@gmail.com>
2021-11-02 00:50:34 +0500

    Refactoring a rate-button component

--- src/components/rate-button/rate-button.js
+++ src/components/rate-button/rate-button.js
@@ -4,14 +4,15 @@ import $ from 'jquery';
 class RateButton {
   constructor($component) {
     this.$component = $component;
-    this.stars = Array.from($($component).find('.rate-button__star'));
-    this.rate = this.getRate();
+    this.rate = Number($component.attr('data-rate'));
+    this.stars = Array.from($('.js-rate-button__star', $component));
+    this.setRate(this.rate);
     this.attachEventHandlers();
   }
 
   attachEventHandlers() {
-    this.stars.forEach(($star) => {
-      $($star)
+    this.stars.forEach((star) => {
+      $(star)
         .on('click', (event) => this.onStarClick(event.target))
         .on('mouseover', (event) => this.onStarHover(event.target));
     });
@@ -21,40 +22,37 @@ class RateButton {
   setRate(rate, isChanging = true) {
     if (isChanging) {
       this.rate = rate;
+      this.$component.attr('data-rate', rate);
     }
 
     this.setStars(rate);
     this.setUnstars(rate);
   }
 
-  onStarClick($star) {
-    const rate = this.getStarRate($star);
+  onStarClick(star) {
+    const rate = this.getStarRate(star);
     this.setRate(rate);
   }
 
-  onStarHover($star) {
-    const rate = this.getStarRate($star);
+  onStarHover(star) {
+    const rate = this.getStarRate(star);
     this.setRate(rate, false);
   }
 
   setStars(rate) {
-    this.stars.slice(0, rate).forEach(($star) => {
-      $($star).html('star');
+    this.stars.slice(0, rate).forEach((star) => {
+      $(star).html('star');
     });
   }
 
   setUnstars(rate) {
-    this.stars.slice(rate).forEach(($star) => {
-      $($star).html('star_border');
+    this.stars.slice(rate).forEach((star) => {
+      $(star).html('star_border');
     });
   }
 
-  getStarRate($star) {
-    return this.stars.indexOf($star) + 1;
-  }
-
-  getRate() {
-    return this.stars.reduce((rate, $star) => rate + Number($($star).html() === 'star'), 0);
+  getStarRate(star) {
+    return this.stars.indexOf(star) + 1;
   }
 }
 
