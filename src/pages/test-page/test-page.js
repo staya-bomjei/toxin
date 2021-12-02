8527dff3b6df435aac07723068a4cfaca9b8671c
Denis Beschastnykh <beschastnykh.denis@gmail.com>
2021-12-02 16:50:14 +0500

    Refactoring bullet-list component, update test-page

--- src/pages/test-page/test-page.js
+++ src/pages/test-page/test-page.js
@@ -2,5 +2,6 @@ import '../../components/color-sample/color-sample';
 import '../../components/text-sample/text-sample';
 import '../../components/text-field/text-field';
 import '../../components/button/button';
+import '../../components/bullet-list/bullet-list';
 
 import './test-page.scss';
