663bf50a9546917be8c0c2ba0852590e9808132d
Denis Beschastnykh <beschastnykh.denis@gmail.com>
2021-12-02 15:34:38 +0500

    Update test-page

--- src/pages/test-page/test-page.js
+++ src/pages/test-page/test-page.js
@@ -1,4 +1,5 @@
 import '../../components/color-sample/color-sample';
 import '../../components/text-sample/text-sample';
+import '../../components/text-field/text-field';
 
 import './test-page.scss';
