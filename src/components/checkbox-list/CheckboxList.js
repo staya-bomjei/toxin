88d67db9ebc39056768372c09dceab398c007961
Denis Beschastnykh <beschastnykh.denis@gmail.com>
2022-01-20 21:03:32 +0500

    Remove useless "isExpanded", remove out of component handler #47

--- src/components/checkbox-list/CheckboxList.js
+++ src/components/checkbox-list/CheckboxList.js
@@ -2,8 +2,6 @@ import $ from 'jquery';
 
 import {
   BUTTON_SELECTOR,
-  LIST_SELECTOR,
-  CHECKBOX_LIST_EXPANDED,
   CHECKBOX_LIST_OPEN,
 } from './const';
 
@@ -11,29 +9,18 @@ class CheckboxList {
   constructor($component) {
     this.$component = $component;
     this.$button = $(BUTTON_SELECTOR, $component);
-    this.$list = $(LIST_SELECTOR, $component);
   }
 
   init() {
-    if (!this.$component.hasClass(CHECKBOX_LIST_EXPANDED)) {
-      this.$list.css('z-index', Number(this.$list.css('z-index')) + 1);
-    }
     this._attachEventHandlers();
   }
 
   _attachEventHandlers() {
-    $(document).on('click', (event) => this._handleOutOfComponentClick(event));
     this.$button.on('click', () => this._handleButtonClick());
   }
 
   _handleButtonClick() {
     this.$component.toggleClass(CHECKBOX_LIST_OPEN);
-  }
-
-  _handleOutOfComponentClick({ target }) {
-    if (this.$component.has(target).length === 0) {
-      this.$component.removeClass(CHECKBOX_LIST_OPEN);
-    }
   }
 }
 
--- src/components/checkbox-list/const.js
+++ src/components/checkbox-list/const.js
@@ -1,14 +1,10 @@
 const CHECKBOX_LIST_SELECTOR = '.js-checkbox-list';
 const BUTTON_SELECTOR = '.js-checkbox-list__button';
-const LIST_SELECTOR = '.js-checkbox-list__list';
 
 const CHECKBOX_LIST_OPEN = 'checkbox-list_open';
-const CHECKBOX_LIST_EXPANDED = 'checkbox-list_expanded';
 
 export {
   CHECKBOX_LIST_SELECTOR,
   BUTTON_SELECTOR,
-  LIST_SELECTOR,
   CHECKBOX_LIST_OPEN,
-  CHECKBOX_LIST_EXPANDED,
 };
